74aab46b7f7b49235ac29672cb447a0a0c4344d1
Lohan Luiz <lohanluiz12@gmail.com>
2020-12-22 09:42:41 -0400

    configuring the routes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => {
-  <h1>Dashboard</h1>;
-};
+const Dashboard: React.FC = () => <div>Dashboard</div>;
 
 export default Dashboard;
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const Repository: React.FC = () => {
-  <h1>Repository</h1>;
-};
+const Repository: React.FC = () => <div>Repository</div>;
 
 export default Repository;
