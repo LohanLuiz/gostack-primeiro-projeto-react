eca96a260cf8ad4c87a83bf57b749bdc4d248112
Lohan Luiz <lohanluiz12@gmail.com>
2020-12-23 09:39:01 -0400

    logo progress

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import logoImg from '../../assets/logo.svg';
+
 import { Title } from './styles';
 
 const Dashboard: React.FC = () => (
   <>
+    <img src={logoImg} alt="GitHub Explorer" />
     <Title> Explore repositórios no Github</Title>
-    <h1> Explore repositórios no Github</h1>
   </>
 );
 
