99d5dc617d57762d594b50d0417c11adb2b5566f
Lohan Luiz <lohanluiz12@gmail.com>
2020-12-23 08:49:12 -0400

    adding styled components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <div>Dashboard</div>;
+const Dashboard: React.FC = () => (
+  <>
+    <h1> Explore repositórios no Github</h1>
+    <h1> Explore repositórios no Github</h1>
+  </>
+);
 
 export default Dashboard;
