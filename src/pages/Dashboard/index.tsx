dcc11bb0ab51d0ca9031e2ea1aef171d103ebe5b
Lohan Luiz <lohanluiz12@gmail.com>
2020-12-23 09:50:32 -0400

    progress

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title } from './styles';
+import { Title, Form } from './styles';
 
 const Dashboard: React.FC = () => (
   <>
     <img src={logoImg} alt="GitHub Explorer" />
     <Title> Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
   </>
 );
 
