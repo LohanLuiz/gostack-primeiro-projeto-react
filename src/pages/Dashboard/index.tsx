58d4661a247f396bdeef76407ae83dd4cb0b78af
Lohan Luiz <lohanluiz12@gmail.com>
2020-12-24 16:21:22 -0400

    progress api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="GitHub Explorer" />
-    <Title> Explore repositórios no Github</Title>
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
 
-    <Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
+  // function handleAddRepository() {}
 
-    <Repositories>
-      <a href="teste">
-        <img
-          src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=4fcc8ca9672eeb41ea800271831b7c687bc17054&v=4"
-          alt="Diego Fernandes"
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title> Explore repositórios no Github</Title>
+
+      <Form>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
         />
-        <div>
-          <strong>Rocketseat/unform</strong>
-          <p>Easy peasy highly scalable React JS</p>
-        </div>
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-);
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=4fcc8ca9672eeb41ea800271831b7c687bc17054&v=4"
+            alt="Diego Fernandes"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Easy peasy highly scalable React JS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
